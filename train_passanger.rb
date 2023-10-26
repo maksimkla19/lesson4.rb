e50b9779b8555557d55fc9df6bc36648c6751a18
maksimkla19 <68090252+maksimkla19@users.noreply.github.com>
2023-10-26 18:56:41 +0600

    Add files via upload
    passanger.rb wrong

---
+++ train_passanger.rb
@@ -0,0 +1,15 @@
+
+require_relative 'train'
+
+class PassengerTrain < Train
+
+  def initialize(number)
+    super(number, :passenger)
+  end
+
+  def wagon_add(wagon)
+    return unless self.type == wagon.type
+    super(wagon)
+  end
+
+end
